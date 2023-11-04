f0b641f25c3dd967854b59e0b63682744e98b976
erfanzar <erfanzare82@yahoo.com>
2023-11-04 23:09:49 +0330

    making imports more efficient

--- fjformer/utils.py
+++ fjformer/utils.py
@@ -13,3 +13,46 @@ def count_num_params(_p):
 
 def count_params(_p):
     print('\033[1;31mModel Contain : ', count_num_params(_p) / 1e9, ' Billion Parameters')
+
+
+class JaxRNG(object):
+    @classmethod
+    def from_seed(cls, seed):
+        return cls(jax.random.PRNGKey(seed))
+
+    def __init__(self, rng):
+        self.rng = rng
+
+    def __call__(self, keys=None):
+        if keys is None:
+            self.rng, split_rng = jax.random.split(self.rng)
+            return split_rng
+        elif isinstance(keys, int):
+            split_rngs = jax.random.split(self.rng, num=keys + 1)
+            self.rng = split_rngs[0]
+            return tuple(split_rngs[1:])
+        else:
+            split_rngs = jax.random.split(self.rng, num=len(keys) + 1)
+            self.rng = split_rngs[0]
+            return {key: val for key, val in zip(keys, split_rngs[1:])}
+
+
+def init_rng(seed):
+    global jax_utils_rng
+    jax_utils_rng = JaxRNG.from_seed(seed)
+
+
+def next_rng(*args, **kwargs):
+    global jax_utils_rng
+    return jax_utils_rng(*args, **kwargs)
+
+
+class GenerateRNG:
+    def __init__(self, seed: int = 0):
+        self.seed = seed
+        self.rng = jax.random.PRNGKey(seed)
+
+    def __next__(self):
+        while True:
+            self.rng, ke = jax.random.split(self.rng, 2)
+            return ke
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fjformer",
-    version='0.0.1',
+    version='0.0.6',
     author="Erfan Zare Chavoshi",
     author_email="erfanzare82@yahoo.com",
     long_description=long_description,
@@ -15,20 +15,18 @@ setuptools.setup(
     url="https://github.com/erfanzar/",
     packages=setuptools.find_packages(),
     install_requires=[
-        "numpy",
+        "chex~=0.1.7",
+        "typing",
         "jax>=0.4.10",
         "transformers>=4.34.0",
-        "typing~=3.7.4.3",
-        "numpy",
         "flax>=0.7.1",
         "optax>=0.1.7",
-        "einops>=0.6.1",
-        "msgpack>=1.0.5",
+        "einops",
+        "msgpack",
         "ml_collections",
-        "torch>=2.0.0",
         "einops",
         "datasets",
-        "IPython>=8.17.2"
+        "IPython"
     ],
     python_requires=">=3.7",
     license='Apache License 2.0',
@@ -37,7 +35,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
