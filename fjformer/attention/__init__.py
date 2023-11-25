0f5045074dd9739cb62d21941827b2230cfd080b
erfanzar <erfanzare82@yahoo.com>
2023-11-25 18:00:07 +0330

    Better Attention Added

--- fjformer/attention/__init__.py
+++ fjformer/attention/__init__.py
@@ -1,3 +1,5 @@
 from .efficient_attention import efficient_attention
 from .flash_attention_0 import dot_product_attention_multihead, dot_product_attention_multiquery, \
     dot_product_attention_queries_per_head
+from .flash_attention import ring_attention, ring_attention_standard, ring_flash_attention_gpu, \
+    ring_flash_attention_tpu, blockwise_ffn, blockwise_attn
