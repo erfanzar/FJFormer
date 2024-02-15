e54e150f422480c678f32b11575b07baedba1e37
Erfan Zare Chavoshi <59269023+erfanzar@users.noreply.github.com>
2024-02-15 15:00:03 +0330

    Update implicit_array.py

--- src/fjformer/xrapture/implicit_array.py
+++ src/fjformer/xrapture/implicit_array.py
@@ -39,7 +39,7 @@ from itertools import chain
 import jax
 from jax.api_util import flatten_fun_nokwargs
 from jax import core
-import jax.linear_util as lu
+import jax.extend.linear_util as lu
 from jax import tree_util
 
 
