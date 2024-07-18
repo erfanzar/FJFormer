124b574a35ff97b88fe45399e9e3c56b82c72c5a
erfanzar <erfanzare82@yahoo.com>
2024-07-18 15:41:22 +0330

    Updating to version `0.0.71`

--- src/fjformer/__init__.py
+++ src/fjformer/__init__.py
@@ -4,14 +4,10 @@ from fjformer import monitor as monitor
 from fjformer import optimizers as optimizers
 from fjformer import pallas_operations as pallas_operations
 from fjformer.checkpoint import CheckpointManager as CheckpointManager
-from fjformer.sharding import (
-    create_mesh as create_mesh,
-)
+from fjformer.sharding import create_mesh as create_mesh
 from fjformer.sharding import flatten_tree as flatten_tree
 from fjformer.sharding import get_jax_mesh as get_jax_mesh
-from fjformer.sharding import (
-    get_metrics as get_metrics,
-)
+from fjformer.sharding import get_metrics as get_metrics
 from fjformer.sharding import (
     get_names_from_partition_spec as get_names_from_partition_spec,
 )
